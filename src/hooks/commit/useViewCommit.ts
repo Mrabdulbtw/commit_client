de948c06f871d19ae16bef56cb3d0afbd68160f8
Abdul <abdulofficialbtw@gmail.com>
2026-06-20 20:20:56 +0530

    added url

--- src/hooks/commit/useViewCommit.ts
+++ src/hooks/commit/useViewCommit.ts
@@ -2,17 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export const useViewCommit = (owner: string, repo: string, sha: string) => {
-
   const getCommit = async (owner: string, repo: string, sha: string) => {
-    const { data } = await axios.get(`http://localhost:5000/api/v1/commit/view/${owner}/${repo}/${sha}`);
+    const { data } = await axios.get(
+      `${process.env.BACKEND_URL||"http://localhost:5000/api/v1/commit/view"
+      }/${owner}/${repo}/${sha}`
+    );
     return data;
   };
-  
+
   const { data, isLoading, error } = useQuery({
     queryKey: ["commit", owner, repo, sha],
     queryFn: () => getCommit(owner, repo, sha),
     enabled: !!owner && !!repo && !!sha,
-    staleTime:1000 * 60 * 5
+    staleTime: 1000 * 60 * 5,
   });
 
   return {
